b469747acc559678ba752440d7e3a145a6250313
Connor McCarthy <mccarthyrcconnor@gmail.com>
2025-02-11 00:01:46 -0500

    set atricles date and page

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
+from data import Articles
 
 app = Flask(__name__)
+
+Articles = Articles()
 
 @app.route('/')
 def index():
@@ -10,6 +13,13 @@ def index():
 def about():
     return render_template('about.html')
 
+@app.route('/articles')
+def articles():
+    return render_template('articles.html', articles=Articles)
+
+@app.route('/articles/<string:id>/')
+def articles(id):
+    return render_template('articles.html', id=id)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ data.py
@@ -0,0 +1,28 @@
+def Articles():
+    articles =[
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, fermentum turpis nec, posuere turpis',
+            'author': 'Connor McCarty',
+            'create_date': '02-10-2025'
+
+        },
+                {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, fermentum turpis nec, posuere turpis',
+            'author': 'John Doe',
+            'create_date': '02-10-2025'
+
+        },
+                {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, fermentum turpis nec, posuere turpis',
+            'author': 'Brad Traversy',
+            'create_date': '02-10-2025'
+
+        }
+    ]
+    return articles
